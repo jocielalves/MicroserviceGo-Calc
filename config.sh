96c748e11717b652e7446c95465ab1e214edc12d
Jociel Alves <jocielalvesdejesus@gmail.com>
2022-09-21 10:06:14 -0300

    atualizando

--- config.sh
+++ config.sh
@@ -1,8 +1,6 @@
-version = "3.7"
-
 cd ../../..
-wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/$version/gatling-charts-highcharts-bundle-$version-bundle.zip
-unzip gatling-charts-highcharts-bundle-$version-bundle.zip
-cp -a TestSimulation.scala gatling-charts-highcharts-bundle-$version/user-files/simulations/computerdatabase/
-cd gatling-charts-highcharts-bundle-$version/bin/
+wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.8.4/gatling-charts-highcharts-bundle-3.8.4-bundle.zip
+unzip gatling-charts-highcharts-bundle-3.8.4-bundle.zip
+cp -a TestSimulation.scala gatling-charts-highcharts-bundle-3.8.4/user-files/simulations/computerdatabase/
+cd gatling-charts-highcharts-bundle-3.8.4/bin/
 ./gatling.sh -s computerdatabase.TestSimulation
