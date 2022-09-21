55a3f32f56c67e50cbd2f8c43574b348f7567124
Jociel Alves <jocielalvesdejesus@gmail.com>
2022-09-21 10:21:19 -0300

    teste

--- config.sh
+++ config.sh
@@ -1,4 +1,3 @@
-cd ../../..
 wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.8.4/gatling-charts-highcharts-bundle-3.8.4-bundle.zip
 unzip gatling-charts-highcharts-bundle-3.8.4-bundle.zip
 cp -a TestSimulation.scala gatling-charts-highcharts-bundle-3.8.4/user-files/simulations/computerdatabase/
